8bebcd973408f7c7b0229c3dd7d4ff7933235755
whythatt <zakrogo513@gmail.com>
2024-10-31 09:32:01 +0300

    added links_list.txt

--- main.py
+++ main.py
@@ -11,66 +11,66 @@ from selenium.webdriver.support.ui import WebDriverWait
 driver = webdriver.Chrome()  # Убедитесь, что chromedriver находится в PATH
 
 # URL для загрузки
-url = "https://pikabu.ru/community/Bank/search?t=%D0%91%D0%B0%D0%BD%D0%BA&st=2"
-driver.get(url)
+# url = "https://pikabu.ru/community/Bank/search?t=%D0%91%D0%B0%D0%BD%D0%BA&st=2"
+# driver.get(url)
 
 # Прокрутка страницы вниз для подгрузки постов
-last_height = driver.execute_script("return document.body.scrollHeight")
+# last_height = driver.execute_script("return document.body.scrollHeight")
 
-while True:
-    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-
-    # Явное ожидание загрузки новых постов
-    WebDriverWait(driver, 50).until(
-        lambda d: d.execute_script("return document.readyState") == "complete"
-    )
-
-    time.sleep(5)
-
-    # Получаем новую высоту страницы
-    new_height = driver.execute_script("return document.body.scrollHeight")
-
-    if new_height == last_height:
-        break  # Прокрутка завершена, если высота не изменилась
-
-    last_height = new_height
-
-# Получаем HTML-код страницы после загрузки всех постов
-page_source = driver.page_source
-soup = BeautifulSoup(page_source, "html.parser")
-
-# Извлечение всех ссылок на комментарии
-comment_links = soup.find_all("a", class_="story__comments-link story__to-comments")
-links_list = [link["href"] for link in comment_links]
+# while True:
+#     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+#
+#     # Явное ожидание загрузки новых постов
+#     WebDriverWait(driver, 50).until(
+#         lambda d: d.execute_script("return document.readyState") == "complete"
+#     )
+#
+#     time.sleep(5)
+#
+#     # Получаем новую высоту страницы
+#     new_height = driver.execute_script("return document.body.scrollHeight")
+#
+#     if new_height == last_height:
+#         break  # Прокрутка завершена, если высота не изменилась
+#
+#     last_height = new_height
+#
+# # Получаем HTML-код страницы после загрузки всех постов
+# page_source = driver.page_source
+# soup = BeautifulSoup(page_source, "html.parser")
+#
+# # Извлечение всех ссылок на комментарии
+# comment_links = soup.find_all("a", class_="story__comments-link story__to-comments")
+# links_list = [link["href"] for link in comment_links]
 
 comments_list = []
-
 # Параллельная обработка ссылок на комментарии
-for link in links_list:
-    driver.get(link)
+with open("links_list.txt", "r") as links_list:
+    for link in links_list:
+        driver.get(link)
 
-    # Ожидание кнопки "Показать все комментарии" и клик по ней
-    wait = WebDriverWait(driver, 50)
-    show_comments_button = wait.until(
-        EC.element_to_be_clickable((By.CLASS_NAME, "comment__more"))
-    )
-    ActionChains(driver).move_to_element(show_comments_button).click().perform()
+        # Ожидание кнопки "Показать все комментарии" и клик по ней
+        wait = WebDriverWait(driver, 50)
+        show_comments_button = wait.until(
+            EC.element_to_be_clickable((By.CLASS_NAME, "comment__more"))
+        )
+        ActionChains(driver).move_to_element(show_comments_button).click().perform()
 
-    # Ожидание подгрузки комментариев
-    WebDriverWait(driver, 50).until(
-        lambda d: d.execute_script("return document.readyState") == "complete"
-    )
+        # Ожидание подгрузки комментариев
+        WebDriverWait(driver, 50).until(
+            lambda d: d.execute_script("return document.readyState") == "complete"
+        )
 
-    # Получаем HTML-код страницы после загрузки всех комментариев
-    page_source = driver.page_source
-    soup = BeautifulSoup(page_source, "html.parser")
+        # Получаем HTML-код страницы после загрузки всех комментариев
+        page_source = driver.page_source
+        soup = BeautifulSoup(page_source, "html.parser")
 
-    # Извлечение всех комментариев
-    comments_user = soup.find_all("div", class_="comment__user")
+        # Извлечение всех комментариев
+        comments_user = soup.find_all("div", class_="comment__user")
 
-    for comment in comments_user:
-        username = comment.find("span", class_="user__nick")
-        comments_list.append(username.get_text(strip=True))
+        for comment in comments_user:
+            username = comment.find("span", class_="user__nick")
+            comments_list.append(username.get_text(strip=True))
 
 # Закрываем драйвер
 driver.quit()
